2bfc8c05f58bc548f509317e13957141d2ad732e
Ivan Pakhomov <ip1133@ya.ru>
2024-11-27 01:33:40 +0300

    refactor: replace input form, add icons

--- src/components/OrderForm.jsx
+++ src/components/OrderForm.jsx
@@ -1,20 +1,22 @@
 import { Dropdown } from 'primereact/dropdown'
 import { InputText } from 'primereact/inputtext'
 import { Button } from 'primereact/button'
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
+import { ToastContainer } from 'react-toastify'
+import { notify } from '../utils/Notification/notification.js'
+import 'react-toastify/dist/ReactToastify.css'
 
 import { useState, useContext, useEffect } from 'react'
-import { ToastContainer } from 'react-toastify'
+
 import OrdersContext from '../context/OrdersContext.jsx'
 import { getOrdersDataApi } from '../api/getOrdersDataApi.js'
 import { getUsersDataApi } from '../api/getUsersDataApi.js'
 import { postOrdersDataApi } from '../api/postOrdersDataApi.js'
 import { checkUser } from '../utils/checkUser.js'
 import { setDocs } from '../utils/setDocs.js'
-import { notify } from '../utils/Notification/notification.js'
-import 'react-toastify/dist/ReactToastify.css'
-import 'primereact/resources/themes/saga-blue/theme.css'
-import 'primereact/resources/primereact.min.css'
-import 'primeicons/primeicons.css'
 
 function OrderForm() {
   const [data, setData] = useState({ username: '', doctitle: '' })
@@ -63,29 +65,39 @@ function OrderForm() {
     <div className="order-form">
       <h2>Заявка на документ</h2>
       <form onSubmit={handleSubmit}>
-        <Dropdown
-          value={data.username}
-          onChange={handleChange}
-          name="username"
-          options={
-            users.length
-              ? users.map((user) => user.username)
-              : console.log('Нет юзеров')
-          }
-          optionLabel="name"
-          placeholder="ФИО конструктора"
-          className="dropdown-name"
-        />
-        <InputText
-          autoComplete="off"
-          placeholder="Название документа"
-          name="doctitle"
-          value={data.doctitle}
-          onChange={handleChange}
-        />
+        <div className="p-inputgroup flex-1">
+          <span className="p-inputgroup-addon">
+            <i className="pi pi-user"></i>
+          </span>
+          <Dropdown
+            value={data.username}
+            onChange={handleChange}
+            name="username"
+            options={
+              users.length
+                ? users.map((user) => user.username)
+                : console.log('Нет юзеров')
+            }
+            optionLabel="name"
+            placeholder="ФИО конструктора"
+            className="dropdown-name"
+          />
+        </div>
+        <div className="p-inputgroup flex-1">
+          <span className="p-inputgroup-addon">
+            <i className="pi pi-file-pdf"></i>
+          </span>
+          <InputText
+            autoComplete="off"
+            placeholder="Название документа"
+            name="doctitle"
+            value={data.doctitle}
+            onChange={handleChange}
+          />
+        </div>
         <Button label="Отправить" severity="secondary" type="submit" />
       </form>
-      <ToastContainer />
+      <ToastContainer draggable />
     </div>
   )
 }
