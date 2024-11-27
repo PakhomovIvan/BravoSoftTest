36be574037c61c0f919adf6008806f0e756c1cf7
Ivan Pakhomov <ip1133@ya.ru>
2024-11-28 00:53:50 +0300

    del: settimeout

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,9 @@ import OrdersContext from './context/OrdersContext'
 import { useState } from 'react'
 import MainTabs from './components/Tabs/MainTabs'
 import './App.css'
-import { getUsersDataApi } from './api/getUsersDataApi'
 
 function App() {
   const [orders, setOrders] = useState()
-
-  setInterval(async () => await getUsersDataApi(), 30000) // чтобы API не перестало отдавать данные по истечении 50с неактивности
-
   return (
     <OrdersContext.Provider value={{ orders, setOrders }}>
       <div className="App">
